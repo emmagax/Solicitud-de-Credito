738b85a5ffef5069c78fbeaf07ae8733689d40e2
Emma Gaxiola <emma.gaxiol@gmail.com>
2024-09-27 18:44:43 -0600

    deploy

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route, } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import homeApp from './Home'; // Import your components
 
 const App = () => {
     return (
         <BrowserRouter basename="/Solicitud-de-Credito">
-            <Route path="/" exact component={homeApp} /> { }
+            <Routes>
+                <Route path="/" exact component={homeApp} />
+            </Routes>
+
         </BrowserRouter>
     );
 };

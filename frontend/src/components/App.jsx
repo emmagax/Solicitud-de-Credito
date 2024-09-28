3faf0490a10b712bef216647d8091ad25a28eebf
Emma Gaxiola <emma.gaxiol@gmail.com>
2024-09-27 18:42:39 -0600

    deploy

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, } from 'react-router-dom';
 import homeApp from './Home'; // Import your components
 
 const App = () => {
     return (
         <BrowserRouter basename="/Solicitud-de-Credito">
-            <Switch>
-                <Route path="/" exact component={homeApp} /> { }
-            </Switch>
+            <Route path="/" exact component={homeApp} /> { }
         </BrowserRouter>
     );
 };

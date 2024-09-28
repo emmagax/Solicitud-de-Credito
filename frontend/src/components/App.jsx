efe4b6b3c9b1bb0cbbab329d113fd0026e0986ae
Emma Gaxiola <emma.gaxiol@gmail.com>
2024-09-27 18:40:51 -0600

    deploy

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,20 +1,14 @@
-import React, { useState } from 'react';
-import CreditRequestForm from './CreditRequestForm';
-import ResultDisplay from './ResultsDisplay';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import homeApp from './Home'; // Import your components
 
 const App = () => {
-    const [result, setResult] = useState(null);
-
-    const handleResult = (result) => {
-        setResult(result);
-    };
-
     return (
-        <div>
-            <h1>Simulador de Solicitud de Crédito</h1>
-            <CreditRequestForm onResult={handleResult} />
-            {result && <ResultDisplay result={result} />}
-        </div>
+        <BrowserRouter basename="/Solicitud-de-Credito">
+            <Switch>
+                <Route path="/" exact component={homeApp} /> { }
+            </Switch>
+        </BrowserRouter>
     );
 };
 
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import CreditRequestForm from './CreditRequestForm';
+import ResultDisplay from './ResultsDisplay';
+
+const homeApp = () => {
+    const [result, setResult] = useState(null);
+
+    const handleResult = (result) => {
+        setResult(result);
+    };
+
+    return (
+        <div>
+            <h1>Simulador de Solicitud de Crédito</h1>
+            <CreditRequestForm onResult={handleResult} />
+            {result && <ResultDisplay result={result} />}
+        </div>
+
+    );
+
+};
+
+
+
+export default homeApp;
